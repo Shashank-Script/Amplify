f03267cf28468d652312cac7e838edf78818c29c
Shashank-Script <shashankgupta26122@gmail.com>
2025-08-04 00:10:42 +0530

    Backend setup

---
+++ user service/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./database.js"
+import userRoutes from "./route.js"
+
+dotenv.config()
+connectDB();
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.use("/api/v1", userRoutes)
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Server Running....")
+})
+
+app.listen(3000, () => {
+    console.log(`server is running on port: ${port}`)
+})
